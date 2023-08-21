2a8b9fd46f6b4225539fbdbff4797496d0319103
AlRezepov <alexrezepovjr@mail.ru>
2023-08-21 09:30:51 +0700

    Add Homework_03

---
+++ 03/task_01/task_01.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+class smart_array
+{
+public:
+	smart_array(int size) {
+		this->size = size;
+		arr = new int[size];
+	};
+
+	void add_element(int element) {
+		if (index >= size)
+		{
+			throw std::runtime_error("Arr is full");
+		}
+		arr[index] = element;
+		index++;
+	}
+
+	int get_element(int index) {
+		if (index < size && index >= 0)
+		{
+			return arr[index];
+		}
+		else {
+			throw std::runtime_error("Index isn't correct");
+		} 
+	}
+
+	~smart_array() {
+		delete [] arr;
+	};
+
+private:
+	int size;
+	int* arr;
+	int index = 0;
+};
+
+int main()
+{
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(1) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
---
+++ 03/task_02/task_02.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+class smart_array
+{
+public:
+	smart_array(int size) {
+		this->size = size;
+		arr = new int[size];
+	};
+
+	void add_element(int element) {
+		if (index >= size)
+		{
+			throw std::runtime_error("Arr is full");
+		}
+		arr[index] = element;
+		index++;
+	}
+
+	int get_element(int index) {
+		if (index < size && index >= 0)
+		{
+			return arr[index];
+		}
+		else {
+			throw std::runtime_error("Index isn't correct");
+		}
+	}
+
+	smart_array& operator=(const smart_array& other) {
+		if (this != &other) {
+			delete[] arr;
+			size = other.size;      // Копирование размера
+
+			arr = new int[size];    // Выделение новой памяти
+			for (int i = 0; i < size; ++i) {
+				arr[i] = other.arr[i];  // Копирование элементов
+			}
+		}
+		return *this;
+	}
+
+	void print() {
+		for (int i = 0; i < size; i++)
+		{
+			std::cout << arr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	~smart_array() {
+		delete[] arr;
+	};
+
+private:
+	int size;
+	int* arr;
+	int index = 0;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+
+		std::cout << "Первый массив: \n";
+		arr.print();
+
+		smart_array new_array(2);
+		new_array.add_element(44);
+		new_array.add_element(34);
+
+		std::cout << "Второй массив: \n";
+		new_array.print();
+
+		arr = new_array;
+
+		std::cout << "Arr = new_array: \n";
+		arr.print();
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
