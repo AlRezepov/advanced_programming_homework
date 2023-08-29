0b65b732bec48ffa847835d1f2dec65df0ac5180
AlRezepov <alexrezepovjr@mail.ru>
2023-08-29 09:40:16 +0700

    Add Homework_05

---
+++ 05/task_01/task_01.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+T squaring(T a) {
+    return a * a;
+}
+
+template <typename U>
+void squaring(std::vector<U>& a) {
+    for (auto& i : a) {
+        i = i * i;
+    }
+}
+
+int main()
+{
+    auto a = 0;
+
+    std::cout << "[IN]: ";
+    std::cin >> a;
+    std::cout << "[OUT]: ";
+    std::cout << squaring (a) << "\n";
+    
+    std::vector <int> A = { -1, 4, 8 };
+
+    std::cout << "[IN]: ";
+    for (auto& i : A)
+    {
+        std::cout << i << ", ";
+    }
+    std::cout << "\n";
+    std::cout << "[OUT]: ";
+    squaring(A);
+    for (auto& i : A)
+    {
+        std::cout << i << ", ";
+    }
+    return 0;
+}
---
+++ 05/task_02/task_02.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class T>
+class table
+{
+public:
+    table(int rows, int cols) : rows(rows), cols(cols), A(rows, std::vector<T>(cols)) {}
+
+    std::vector<T>& operator[](int row) {
+        return A[row];
+    }
+
+    const std::vector<T>& operator[](int row) const {
+        return A[row];
+    }
+
+    int Size() const {
+        return rows * cols;
+    }
+
+private:
+    std::vector<std::vector<T>> A;
+    int rows;
+    int cols;
+};
+
+int main()
+{
+    auto test = table<int>(2, 3);
+    test[0][0] = 4;
+    std::cout << test[0][0] << std::endl; // Output: 4
+    std::cout << test.Size() << std::endl; // Output: 6
+
+    return 0;
+}
