769152a600a196061b121be25a3bf729fe0d486a
AlRezepov <alexrezepovjr@mail.ru>
2023-09-10 11:35:38 +0700

    Add Homework_07

---
+++ 07/task_01/task_01.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+void removeDuplicates(std::vector<int>& a) {
+    
+    // Сортировка вектора
+    std::sort(a.begin(), a.end());
+
+    // Использование std::unique
+    auto it = std::unique(a.begin(), a.end());
+
+    //Удаление дубликатов с конца вектора
+    a.erase(it, a.end());
+}
+
+int main() {
+
+    std::vector<int> a = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    // Вывод элементов вектора
+    std::cout << "[IN]: \n";
+    for (const auto& i : a) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    // Удаление дубликатов
+    removeDuplicates(a);
+
+    // Вывод уникальных элементов
+    std::cout << "[OUT]: \n";
+    for (const auto& i : a) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ 07/task_02/task_02.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+
+template <typename T>
+void print_container(const T& container) {
+    for (auto it = container.begin(); it != container.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+    
+    // four one three two. помните почему такой порядок?
+    // Потому что элементы в std::set автоматически сортируются в возрастающем порядке на момент добавления в контейнер. 
+    // В данном случае, строки сортируются в лексикографическом порядке. 
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+}
