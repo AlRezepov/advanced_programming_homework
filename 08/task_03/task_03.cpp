f7ae6aade0ada518caa5a006ccf4f646efce1c99
AlRezepov <alexrezepovjr@mail.ru>
2023-09-11 23:06:48 +0700

    Add Homework_08

---
+++ 08/task_03/task_03.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+template <class T>
+class unique_ptr
+{
+public:
+    unique_ptr(T* ptr) : ptr(ptr) {}
+
+    T operator*() {
+        return *ptr;
+    }
+
+    T* release() {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+
+    unique_ptr(const unique_ptr&) = delete;
+
+    unique_ptr& operator=(const unique_ptr&) = delete;
+
+    ~unique_ptr() {
+        delete ptr;
+        std::cout << "ptr deleted";
+    }
+
+private:
+    T* ptr;
+};
+
+int main()
+{
+    int* ptr = new int(50);
+
+    unique_ptr<int> uniquePtr(ptr);
+
+    int value = *uniquePtr;
+    std::cout << value << "\n";
+
+    int* ptr_02 = uniquePtr.release();
+
+    delete ptr_02;
+
+    return 0;
+}
