c20eccfa1994d80460c9c56ad39f45a41b0d3bcd
AlRezepov <alexrezepovjr@mail.ru>
2023-09-14 13:31:41 +0700

    Add Homework_09

---
+++ 09/task_01/task_01.cpp
@@ -0,0 +1,23 @@
+﻿#include <vector>
+#include <string>
+
+template <typename T>
+void move_vectors(std::vector<T>& a, std::vector<T>& b) {
+    
+    b.clear();
+
+    for (auto&& i : a) {
+        b.push_back(std::move(i));
+    }
+
+    a.clear();
+}
+
+int main() {
+    std::vector<std::string> one = { "test_string1", "test_string2" };
+    std::vector<std::string> two;
+
+    move_vectors(one, two); // one теперь пуст, а two содержит перемещенные строки.
+
+    return 0;
+}
---
+++ 09/task_02/task_02.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <string>
+
+class big_integer {
+private:
+    std::string num;
+
+public:
+ 
+    //Конструктор
+    big_integer(const std::string& s) : num(s) {}
+
+    //Конструктор копирования
+    big_integer(const big_integer& other) : num(other.num) {}
+
+    //Оператор присваивания
+    big_integer& operator=(const big_integer& other) {
+        if (this != &other) {
+            num = other.num;
+        }
+        return *this;
+    }
+
+    // Конструктор перемещения
+    big_integer(std::string&& other) : num(std::move(other)) {}
+
+    // Перемещающий оператор присваивания
+    big_integer& operator=(big_integer&& other) {
+        if (this != &other) {
+            num = std::move(other.num);
+        }
+        return *this;
+    }
+
+    // Оператор сложения двух больших чисел
+    big_integer operator+(const big_integer& other) const {
+        std::string result;
+        int i = num.length() - 1;
+        int j = other.num.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += num[i] - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += other.num[j] - '0';
+                j--;
+            }
+
+            carry = sum / 10;
+            sum %= 10;
+            result.insert(result.begin(), sum + '0');
+        }
+
+        return big_integer(result);
+    }
+
+    // Оператор умножения на число
+    big_integer operator*(int x) const {
+        std::string result;
+        int i = num.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || carry > 0) {
+            int multi = carry;
+            if (i >= 0) {
+                multi += (num[i] - '0') * x;
+                i--;
+            }
+
+            result.insert(result.begin(), (multi % 10) + '0');
+            carry = multi / 10;
+        }
+
+        return big_integer(result);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const big_integer& a) {
+        os << a.num;
+        return os;
+    }
+};
+
+int main() {
+    auto number1 = big_integer("114575");
+    auto number2 = big_integer("78524");
+    auto result = number1 + number2;
+    std::cout << result << "\n";
+
+    auto multiplied = number1 * 5;
+    std::cout << multiplied;
+
+    return 0;
+}
