ff0d2a8f05da0c09d1392916a10f8acaaca20bb1
AlRezepov <alexrezepovjr@mail.ru>
2023-09-23 13:20:08 +0200

    Coursework

--- Coursework/Coursework/Coursework.cpp
+++ Coursework/Coursework/Coursework.cpp
@@ -2,49 +2,84 @@
 #include <map>
 #include <string>
 #include <fstream>
-#include <vector>
+#include <stdexcept>
+#include <sstream>
 
-template <class T>
 class ini_parser
 {
 public:
-	//Конструктор
-	ini_parser(std::string name_file) : name_file(name_file) {
-		std::ifstream ini_file(name_file); // Открываем файл для чтения
-		if (!ini_file) {
-			std::cerr << "Failed to open the file." << std::endl;
-			return 1;
-		}
-		std::string line;
-		while (std::getline(ini_file, line)) {
-			for (size_t i = 0; i < length; i++)
-			{
+    ini_parser(const std::string& name_file) : name_file(name_file) {
+        load_file(name_file);
+    }
 
-			}
-			if (true)
-			{
+    void load_file(const std::string& name_file) {
+        std::ifstream ini_file(name_file); // Открываем файл для чтения
+        if (!ini_file) {
+            throw std::runtime_error("Failed to open the file.");
+        }
+        std::string line;
+        std::string current_section;
+        while (std::getline(ini_file, line)) {
+            if (line.empty() || line[0] == ';') {
+                continue; // Пропускаем пустые строки и строки-комментарии
+            }
+            else if (line[0] == '[' && line[line.length() - 1] == ']') {
+                current_section = line.substr(1, line.length() - 2); // Записываем название секции без учета []
+            }
+            else {
+                int equal_position = line.find('=');
+                if (equal_position != std::string::npos) {
+                    std::string key = line.substr(0, equal_position);
+                    std::string value = line.substr(equal_position + 1);
+                    ini_data[current_section][key] = value;
+                }
+            }
+        }
+        ini_file.close(); // Закрытие файла
+    }
 
-			}
-		}
-	};
-	
-	//Конструктор копирования
+    template <typename T>
+    T get_value(const std::string& section_key) const {
+        int dot_pos = section_key.find('.');
+        if (dot_pos == std::string::npos) {
+            throw std::runtime_error("Invalid section.key format.");
+        }
 
-	//Оператор присваивания
-	
-	~ini_parser() {
-		ini_file.close();
-	};
+        std::string section = section_key.substr(0, dot_pos);
+        std::string key = section_key.substr(dot_pos + 1);
+
+        auto section_iter = ini_data.find(section);
+        if (section_iter != ini_data.end()) {
+            const auto& section_data = section_iter->second;
+            auto key_iter = section_data.find(key);
+            if (key_iter != section_data.end()) {
+                T value;
+                std::istringstream(key_iter->second) >> value;
+                return value;
+            }
+        }
+        throw std::runtime_error("Section.key not found in INI file.");
+    }
 
 private:
-	std::string name_file;
-	std::map <std::string, std::vector<int>> ini_data;
+    std::string name_file;
+    std::map<std::string, std::map<std::string, std::string>> ini_data;
 };
 
+int main() {
+    try {
+        ini_parser parser("demo_file.ini");
+        auto value = parser.get_value<int>("section1.value1");
+        std::cout << "Value: " << value << std::endl;
+        auto value_02 = parser.get_value<std::string>("section2.value2");
+        std::cout << "Value: " << value_02 << std::endl;
+        value_02 = parser.get_value<std::string>("section3.value2");
+        std::cout << "Value: " << value_02 << std::endl;
+        
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
+    return 0;
 }
